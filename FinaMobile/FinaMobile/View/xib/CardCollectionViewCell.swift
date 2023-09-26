3d1f40d90dea8a7c53b354d7728ebe30aae87889
Tinkldeath <dropdeadit@gmail.com>
2023-09-26 15:23:12 +0300

    Lab 1 completed

---
+++ FinaMobile/FinaMobile/View/xib/CardCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CardCollectionViewCell.swift
+//  FinaMobile
+//
+//  Created by Dima on 20.09.23.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
